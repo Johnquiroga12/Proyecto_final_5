9d7f8c102ea4d1b67f7b06c37e1083e472e7d0de
Chimbo-K18 <kevin.chimbo.est@tecazuay.edu.ec>
2023-02-28 12:25:23 -0500

    listas
    asd

--- ParkingCam/src/app/modules/administrador/registra-personal/pages/registra-personal/registra-personal.component.ts
+++ ParkingCam/src/app/modules/administrador/registra-personal/pages/registra-personal/registra-personal.component.ts
@@ -1,11 +1,30 @@
 import { Component,OnInit } from '@angular/core';
 import  Swal from 'sweetalert2';
 import { Router } from '@angular/router';
+import { PersonaService } from 'src/app/services/persona.service';
+import { Persona } from 'src/app/services/class/persona';
 @Component({
   selector: 'app-registra-personal',
   templateUrl: './registra-personal.component.html',
   styleUrls: ['./registra-personal.component.css']
 })
 export class RegistraPersonalComponent{
+
+  personaLista: Persona[] = [];
+
+  constructor(private ClasesPersonas: PersonaService){ }
+
+
+  ngOnInit(): void {
+
+    this.getClasesPersona();
+  }
+
+
+
+  getClasesPersona(){
+    this.ClasesPersonas.getPersonas().subscribe(data => {
+      this.personaLista = data;
+    });}
 
 }
