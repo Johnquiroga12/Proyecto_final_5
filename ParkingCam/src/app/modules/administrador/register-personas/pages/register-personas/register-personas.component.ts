99a983a3fe3f9634c8c9e29310a2d14c6257601c
Johnquiroga12 <100298882+Johnquiroga12@users.noreply.github.com>
2023-03-08 20:24:25 -0500

    validaciones
    john

--- ParkingCam/src/app/modules/administrador/register-personas/pages/register-personas/register-personas.component.ts
+++ ParkingCam/src/app/modules/administrador/register-personas/pages/register-personas/register-personas.component.ts
@@ -10,6 +10,32 @@ import Swal from 'sweetalert2'
 })
 export class RegisterPersonasComponent {
 
+  
+  onKeyDown(event: KeyboardEvent) {
+    this.onInputChange(event);
+  }
+  onInputChange(event: any) {
+    const value = event.target.value;
+    const input = event.target;
+  
+    if (!/^\d+$/.test(value) || value.length < 10) {
+      input.classList.add('invalid');
+    } else {
+      input.classList.remove('invalid');
+    }
+  }
+  InputChange(event: any) {
+    const value = event.target.value;
+    const input = event.target;
+  
+    if (!/^\d+$/.test(value) || value.length < 6) {
+      input.classList.add('invalid');
+    } else {
+      input.classList.remove('invalid');
+    }
+  }
+
+
   persona: Persona = new Persona;
   listaPersonas: Persona[];
   
@@ -24,7 +50,9 @@ export class RegisterPersonasComponent {
   createPersona(){
 
   if(this.persona.cedula === '' || this.persona.nombre === '' || this.persona.apellido === '' ||
-    this.persona.correo === '' || this.persona.celular === '' || this.persona.n_emergencia === ''){
+    this.persona.correo === '' || this.persona.celular === '' || this.persona.n_emergencia === ''
+    ||  this.persona.carrera==null || this.persona.cargo==null || this.persona.jornada==null
+    ){
 
     console.log("Error");
 
@@ -62,6 +90,4 @@ export class RegisterPersonasComponent {
 
     
   }
-
-  re
 }
